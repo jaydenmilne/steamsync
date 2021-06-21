efcb32fc37488e0ebe3b9d203e84213fcb8380bb
David Briscoe <idbrii@gmail.com>
2021-06-21 12:51:53 -0700

    Parse the manifest file to determine preferred exe
    Many games don't have a manifest, but if they do we should respect it.
    
    We look for 'play' which is a preferred label for playing the game. If
    not found, we get the first local file we find because I'd expect
    developers to put the "play game" option first.

---
+++ steamsync-library/src/util.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# LICENSE: AGPLv3. See LICENSE at root of repo
+
+import os
+
+def is_executable_game(game_path):
+    """Is the input path a real file that we have access to?
+
+    is_executable_game(Path) -> bool
+    """
+    try:
+        return game_path.is_file() and os.access(game_path, os.R_OK)
+    except OSError:
+        return False
+
